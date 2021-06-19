7e5ac389531113586b1c8887e060eae5281702f6
Subrat Tripathi <subrat.tripathi@Subrats-MacBook-Pro.local>
2021-06-19 15:03:26 +0530

    Easy 2

---
+++ java-practice-code/src/main/java/Scrabble.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+class Scrabble {
+
+    enum Letter{
+        A(1), E(1), I(1), O(1), U(1),
+        L(1), N(1), R(1), S(1), T(1),
+        D(2), G(2),B(3), C(3), M(3),
+        P(3),F(4), H(4), V(4), W(4),
+        Y(4),K(5),J(8), X(8),
+        Q(10), Z(10);
+        int value;
+        Letter(int value){
+            this.value = value;
+        }
+
+        private int getValue(){
+            return value;
+        }
+    }
+
+    private String word;
+
+    Scrabble(String word) {
+        this.word = word;
+    }
+
+    int getScore() {
+        int score = 0;
+        if(word != null){
+            for(char ch : word.toCharArray()){
+                score = score + Letter.valueOf(String.valueOf(Character.toUpperCase(ch))).getValue();
+            }
+        }
+        return score;
+    }
+
+}
